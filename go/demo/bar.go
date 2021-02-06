a49945ec3ef80a1a23f94de89a63a19e21bcb8ce
Ben Godfrey <bfgodfr@clemson.edu>
2021-02-06 14:43:42 -0500

    Add sentinel bad function for testing golangci-lint. AB#407

--- go/demo/bar.go
+++ go/demo/bar.go
@@ -1,6 +1,18 @@
 package demo
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Bar struct {
 	ID   int    `json:"id" db:"bar_id"`
 	Name string `json:"name" db:"name"`
+}
+
+// Bad is a sentinel function that shall intentionally trigger golangci-lint.
+// FIXME delete this once testing is done.
+func Bad(s string) {
+	x, _ := strconv.Atoi(s)
+	fmt.Println(x)
 }
