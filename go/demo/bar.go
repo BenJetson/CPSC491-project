211ce7b72d2b6333f42d944031da30d7c5de9033
Ben Godfrey <bfgodfr@clemson.edu>
2021-02-06 15:36:50 -0500

    Appease golangci-lint. AB#407

--- go/demo/bar.go
+++ go/demo/bar.go
@@ -1,18 +1,7 @@
 package demo
 
-import (
-	"fmt"
-	"strconv"
-)
-
+// Bar stores arbitrary data, not very useful outside of the demo.
 type Bar struct {
 	ID   int    `json:"id" db:"bar_id"`
 	Name string `json:"name" db:"name"`
-}
-
-// Bad is a sentinel function that shall intentionally trigger golangci-lint.
-// FIXME delete this once testing is done.
-func Bad(s string) {
-	x, _ := strconv.Atoi(s)
-	fmt.Println(x)
 }
--- go/demo/data_store.go
+++ go/demo/data_store.go
@@ -1,4 +1,5 @@
 package demo
 
+// DataStore is the common interface for durable data storage.
 type DataStore interface {
 }
