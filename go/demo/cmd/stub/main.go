d109bde83020e2e6e84eaaac0ad6e8fcf2dc1f7a
Ben Godfrey <bfgodfr@clemson.edu>
2021-02-16 01:39:24 -0500

    Make stub create TIER also. AB#411 AB#410

--- go/demo/cmd/stub/main.go
+++ go/demo/cmd/stub/main.go
@@ -10,25 +10,35 @@ import (
 	"time"
 )
 
-func handle(w http.ResponseWriter, _ *http.Request) {
-	out := struct {
-		Error string `json:"error"`
-	}{
-		"You have successfully hit the API server. " +
-			"Unfortunately, this application is a stub and does nothing.",
-	}
+func makeHandle(tier string) http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		out := struct {
+			Error string `json:"error"`
+		}{
+			fmt.Sprintf(
+				"You have successfully hit the %s tier of the API server. "+
+					"Unfortunately, this application is a stub and does nothing.",
+				tier,
+			),
+		}
 
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Header().Set("Content-Type", "application/json")
 
-	e := json.NewEncoder(w)
-	err := e.Encode(out)
-	if err != nil {
-		log.Panicf("could not encode response, received: %v\n", err)
+		e := json.NewEncoder(w)
+		err := e.Encode(out)
+		if err != nil {
+			log.Panicf("could not encode response, received: %v\n", err)
+		}
 	}
 }
 
 func main() {
+	tier := os.Getenv("TIER")
+	if len(tier) < 1 {
+		log.Fatalln("TIER cannot be blank")
+	}
+
 	portStr := os.Getenv("PORT")
 	if len(portStr) < 1 {
 		log.Fatalln("PORT cannot be blank")
@@ -41,7 +51,7 @@ func main() {
 		log.Fatalf("PORT out of range, must be > 1000")
 	}
 
-	h := http.HandlerFunc(handle)
+	h := makeHandle(tier)
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", port),
 		Handler:        h,
