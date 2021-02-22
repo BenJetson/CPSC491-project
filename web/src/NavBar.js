c88f4df59ecd552820e6e617e26219e1f0df8c92
Cameron Sharpe <sharpe9@clemson.edu>
2021-02-21 20:42:50 -0500

    added a navbar with logout button

--- web/src/NavBar.js
+++ web/src/NavBar.js
@@ -30,7 +30,6 @@ export default function ButtonAppBar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
