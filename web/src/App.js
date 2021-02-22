3e25f6e16e22f64d040f7ace7bd95ac6f59c6ddb
Cameron Sharpe <sharpe9@clemson.edu>
2021-02-21 20:32:15 -0500

    added a navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
+
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from "./NavBar";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <NavBar>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        </NavBar>
       </header>
     </div>
   );
