0d94f0f0bef8f0c5bd2f19bef3548787bd964c11
Cynthia Brazil <rbrazil@clemson.edu>
2021-02-23 17:33:11 -0500

    Prettier updates

--- web/src/components/Login.js
+++ web/src/components/Login.js
@@ -1,16 +1,93 @@
 import React, { useState, useEffect } from "react";
 import {
-    Box,
-    Typography,
-}   from "@material-ui/core";
+  Container,
+  Box,
+  Button,
+  Grid,
+  Typography,
+  Link,
+  Checkbox,
+  FormControlLabel,
+  TextField,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 let Login = () => {
-    return (
-        <Box>
-            <Typography variant="h3">
-                Testing
-            </Typography>
-        </Box>
-    );
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Welcome
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#/forgotpassword" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#/signup" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Box>
+    </Container>
+  );
 };
 export default Login;
