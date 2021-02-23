6e3221aa9371ae35bfc6f36c64f621ee6c7e1ec4
Chloe Lillian Caples <ccaples@clemson.edu>
2021-02-23 03:56:01 +0000

    Apply suggestions from code review

--- db/migrations/V01__init.sql
+++ db/migrations/V01__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-    user_id serial PRIMARY KEY,
+    user_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     last_name varchar(50) NOT NULL,
     first_name varchar(50) NOT NULL,
     email varchar(255) UNIQUE NOT NULL,
