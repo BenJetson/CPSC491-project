fcf8862354e5c5b78958d140fa07042703b53b31
Cameron Sharpe <sharpe9@clemson.edu>
2021-03-11 17:20:25 -0500

    changed import style so Ben is less annoyed

--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import {
+  React,
+  makeStyles,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,9 +32,7 @@ export default function ButtonAppBar() {
             className={classes.menuButton}
             color="inherit"
             aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
+          ></IconButton>
           <Typography variant="h6" className={classes.title}></Typography>
           <Button color="inherit">Logout</Button>
         </Toolbar>
