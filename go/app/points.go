e6bcacbf822e506c6ab27112acf0b589b3de0993
Ben Godfrey <bfgodfr@clemson.edu>
2021-03-16 18:18:59 -0400

    Add documentation comments.

--- go/app/points.go
+++ go/app/points.go
@@ -1,7 +1,13 @@
 package app
 
+// Points describes a particular amount of points, the value of each point,
+// and the organization those points are associated with.
 type Points struct {
-	Amount         int
+	// Amount describes the quantity of points.
+	Amount int
+	// OrganizationID is the identifier of the organization that the points
+	// originated from.
 	OrganizationID int
-	PointValue     Money
+	// PointValue describes the amount of Money that each point is worth.
+	PointValue Money
 }
