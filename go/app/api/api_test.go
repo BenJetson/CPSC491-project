58e15b363bf1a4de0cb28d29ca974064ba64db8d
Ben Godfrey <bfgodfr@clemson.edu>
2021-03-21 16:46:22 -0400

    Structure API server object in a more testable format.

---
+++ go/app/api/api_test.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	logtest "github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/require"
+
+	"github.com/BenJetson/CPSC491-project/go/app"
+)
+
+func newTestAPI(
+	t *testing.T,
+	db app.DataStore,
+) (*Server, *logrus.Logger, *logtest.Hook) {
+
+	logger, hook := logtest.NewNullLogger()
+	logger.Error("Helloerror")
+
+	api, err := NewServer(logger, db, Config{
+		Tier: TierLocal,
+		Port: 8080,
+	})
+	require.NoError(t, err, "failed to instantiate test api server")
+
+	return api, logger, hook
+}
