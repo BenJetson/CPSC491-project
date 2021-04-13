7cb13bc25c8e82b0b8874cae10360dd88c2ef7fe
Chloe Caples <ccaples@clemson.edu>
2021-04-12 22:39:32 -0400

    made changes to table

--- db/migrations/V05__product_table.sql
+++ db/migrations/V05__product_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE product (
     product_id int PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     title text NOT NULL,
-    product_description text NOT NULL,
-    price decimal(7,2)
+    description text NOT NULL,
+    price int NOT NULL
 );
