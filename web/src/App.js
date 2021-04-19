47355f9239cb52ba1cbc43953eebb3e43e101b5e
Ben Godfrey <bfgodfr@clemson.edu>
2021-04-18 22:35:59 -0400

    Update structure of app to reflect changes; install auth context manager.

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { Container, CssBaseline, ThemeProvider } from "@material-ui/core";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
+
+import { AuthProvider, WithUser } from "./api/Auth";
 
 import Application from "./components/Application";
+import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import NavBar from "./components/NavBar";
 import Registration from "./components/Registration";
 import ScrollSpy from "./components/ScrollSpy";
+import NotFound from "./components/NotFound";
+import LoginRequired from "./components/LoginRequired";
 
 import defaultTheme from "./Theme";
 
@@ -21,34 +31,56 @@ let App = () => {
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Router>
-        <ScrollSpy />
-        <Switch>
-          <Route path={"/login"}>
-            <Container>
-              <Login />
-            </Container>
-          </Route>
-          <Route path={"/registration"}>
-            <NavBar />
-            <Container>
-              <Registration />
-            </Container>
-          </Route>
-          <Route path={"/application"}>
-            <NavBar />
-            <Container>
-              <Application
-                status={"pending"}
-                companyName={"Clemson Shipping"}
-                reason={"just too cool"}
-              />
-            </Container>
-          </Route>
-          <Route path={"/home"}>
-            <NavBar />
-            <Container></Container>
-          </Route>
-        </Switch>
+        <AuthProvider>
+          <ScrollSpy />
+          <WithUser>
+            {({ isAuthenticated }) => (
+              <Switch>
+                <Route
+                  exact
+                  // Going to /login always shows the login page.
+                  // If nobody is logged in, going to / will also show the
+                  // login page.
+                  path={isAuthenticated() ? "/login" : ["/login", "/"]}
+                >
+                  <Container>
+                    <Login />
+                  </Container>
+                </Route>
+                <Route>
+                  <NavBar />
+                  <Container>
+                    {(isAuthenticated() && (
+                      <Switch>
+                        <Route path={"/register"}>
+                          <Registration />
+                        </Route>
+                        <Route path={"/apply"}>
+                          <Application
+                            status={"pending"}
+                            companyName={"Clemson Shipping"}
+                            reason={"just too cool"}
+                          />
+                        </Route>
+                        <Route exact path={"/"}>
+                          <Homepage />
+                        </Route>
+                        <Route path={"*"}>
+                          {/* If no route matches, show a not found page. */}
+                          <NotFound />
+                        </Route>
+                      </Switch>
+                    )) || (
+                      // If nobody is logged in, simply show an error message
+                      // on all pages directing to the login page.
+                      <LoginRequired />
+                    )}
+                  </Container>
+                </Route>
+              </Switch>
+            )}
+          </WithUser>
+        </AuthProvider>
       </Router>
     </ThemeProvider>
   );
