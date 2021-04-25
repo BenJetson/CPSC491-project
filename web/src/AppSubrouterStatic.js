dd7ce333d784e267239cbd695a148996d2b54ff4
Cameron Sharpe <sharpe9@clemson.edu>
2021-04-25 16:36:26 -0400

    fixed error

--- web/src/AppSubrouterStatic.js
+++ web/src/AppSubrouterStatic.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import NotFound from "./components/NotFound";
-import NotImplemented from "./components/NotImplemented";
 import FAQ from "./components/FAQ";
 import About from "./components/About";
 const AppSubrouterStatic = () => {
